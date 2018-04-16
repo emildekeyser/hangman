4c7f799ccf20842f29c2e94ceacdc80e1105a28e
Unknown <elewout.vercaeren@gmail.com>
2018-04-16 13:43:43 +0200

    Auto stash before merge of "master" and "origin/master"
    launcher added

--- src/domain/Launcher.java
+++ src/domain/Launcher.java
@@ -3,6 +3,9 @@ package domain;
 public class Launcher {
 	public static void main(String[] args) {
 		
+		String inputValue = JOptionPane.showInputDialog("Welkom!\n Hoe heet je?");
+		Speler speler = new Speler(inputValue);
+		
 	}
 
 }
