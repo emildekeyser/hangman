1ad01bac3791619ea9ec90cdcab845de5667414c
Unknown <dante.cornet@student.ucll.be>
2018-04-17 15:14:58 +0200

    story 12.3

--- src/domain/HintWoord.java
+++ src/domain/HintWoord.java
@@ -11,20 +11,33 @@ public class HintWoord {
 	}
 	
 	public boolean raad(char letter) {
+		woord.indexOf(letter);
+		String result = "";
+		for(int i = 0; i < this.woord.length(); i++) {
+			if(i == woord.indexOf(letter)) {
+				
+			}
+			result += " _";
+		}
 		return true;
 	}
 	
 	public boolean isGeraden() {
-		return true;
+		return false;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "HintWoord [woord=" + woord + "]";
+		String result = "";
+		for(int i = 0; i < this.woord.length(); i++) {
+			result += " _";
+		}
+		return result.substring(1);
 	}
 
 	public String getWoord() {
-		return woord;
+		return this.woord;
+		
 	}
 	
 	
